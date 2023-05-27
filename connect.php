08e9d2b8aaca43df04684bc356299f795289c631
warakornkn98 <126796837+warakornkn98@users.noreply.github.com>
2023-05-27 14:20:45 +0700

    1

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $db = "phpproject";
-    $conn = mysqli_connect($servername,$username,$password,$db);
-    mysqli_set_charset($conn,"utf8");
-?>
